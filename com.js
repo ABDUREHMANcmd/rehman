b87408e0c8a33c0a7c9eb8c82a24ae8380044764
ABDUREHMANcmd <abdurehmanbutt820@gmail.com>
2024-12-07 03:00:52 -0800

    Add files via upload

---
+++ com.js
@@ -0,0 +1,57 @@
+function product_1(){
+    var name=document.getElementById("pro_2").value;
+    var rehman=document.getElementById("text")
+    if(name=="Sana Safinaz Botique"){
+        document.getElementById("img_1").src="image.jpg"
+        rehman.innerHTML="<li style='color:red;font-style:bold;font-size:25px'>Name:- Sana Safinaz Botique </li> <li  style='color:red;font-style:bold;font-size:25px'>Price:- RS:25000 </li><li  style='color:red;font-style:bold;font-size:25px;text-align: justify;'>Sana Safinaz is a renowned Pakistani fashion brand, known for its elegant and luxurious designs. The brand offers a wide range of ready-to-wear and bridal collections that blend traditional craftsmanship with modern trends. With its high-quality fabrics and exquisite stitching, Sana Safinaz has become a symbol of sophistication in the fashion industry.</li> "
+        
+    }
+    else if(name=="Elan Botique"){
+        document.getElementById("img_1").src="img3.webp"
+        rehman.innerHTML="<li  style='color:red;font-style:bold;font-size:25px'>Name:- Elan Botique </li> <li  style='color:red;font-style:bold;font-size:25px'>Price:- RS:30000 </li><li  style='color:red;font-style:bold;font-size:25px;text-align: justify;'>Elan Boutique is a premium fashion brand known for its chic and contemporary designs, offering a blend of traditional and modern styles. It specializes in exquisite couture, luxury prêt, and bridal collections, perfect for every occasion. The boutique is celebrated for its fine craftsmanship, rich fabrics, and attention to detail.</li>"
+    }
+    else if(name=="HSY Botique"){
+        document.getElementById("img_1").src="image4.jpg"
+        rehman.innerHTML="<li  style='color:black;font-style:bold;font-size:25px'>Name:- HSY Botique </li> <li  style='color:black;font-style:bold;font-size:25px'>Price:- RS:22500 </li><li  style='color:black;font-style:bold;font-size:25px;text-align: justify;'>HSY Boutique, founded by renowned designer Hassan Shehryar Yasin, is famous for its luxurious and haute couture creations. The boutique offers a wide range of bespoke bridal, formal, and prêt collections, blending tradition with modern elegance. HSY is celebrated for its intricate designs, high-quality fabrics, and impeccable craftsmanship.</li>"
+    }
+    else if(name=="Zara Shahjahan Botique"){
+        document.getElementById("img_1").src="Screenshot_2023-05-29-23-32-34-73_1c337646f29875672b5a61192b9010f9_800x.webp"
+        rehman.innerHTML="<li  style='color:black;font-style:bold;font-size:25px'>Name:- Zara Shahjahan Botique </li> <li  style='color:black;font-style:bold;font-size:25px'>Price:- RS:32500 </li> <li  style='color:black;font-style:bold;font-size:25px;text-align: justify;'>Zara Shahjahan Boutique is known for its elegant and timeless designs, blending traditional and contemporary styles. The boutique offers a stunning collection of bridal, formal, and ready-to-wear outfits, crafted with intricate details and fine fabrics. Zara Shahjahan's creations reflect sophistication and grace, making it a favorite among fashion enthusiasts.</li>"
+    }
+    else if(name=="Maheen Khan Botique"){
+        document.getElementById("img_1").src="imgg5.jpg"
+        rehman.innerHTML="<li  style='color:black;font-style:bold;font-size:25px'>Name:- Maheen Khan Botique </li> <li  style='color:black;font-style:bold;font-size:25px'>Price:- RS: 5500  </li><li  style='color:black;font-style:bold;font-size:25px;text-align: justify;'>Maheen Khan Boutique is a prestigious fashion label known for its elegant and sophisticated designs. The boutique offers a stunning range of bridal, couture, and ready-to-wear collections, combining traditional craftsmanship with modern aesthetics. Maheen Khan's creations are celebrated for their intricate details, luxurious fabrics, and timeless appeal.</li> "
+    }
+
+
+}
+function product_3(){
+    var name=document.getElementById("pro_5").value;
+    var rehman=document.getElementById("textR")
+    if(name=="Sana Safinaz Botique"){
+        document.getElementById("img_5").src="image.jpg"
+        rehman.innerHTML="<li style='color:black;font-style:bold;font-size:25px'>Name:- Sana Safinaz Botique </li> <li  style='color:black;font-style:bold;font-size:25px'>Price:-RS:25000 </li><li  style='color:black;font-style:bold;font-size:25px;text-align: justify;'>Sana Safinaz is a renowned Pakistani fashion brand, known for its elegant and luxurious designs. The brand offers a wide range of ready-to-wear and bridal collections that blend traditional craftsmanship with modern trends. With its high-quality fabrics and exquisite stitching, Sana Safinaz has become a symbol of sophistication in the fashion industry.</li>"    
+    }
+    else if(name=="Elan Botique"){
+        document.getElementById("img_5").src="img3.webp"
+        rehman.innerHTML="<li  style='color:black;font-style:bold;font-size:25px'>Name:- Elan Botique </li> <li  style='color:black;font-style:bold;font-size:25px'>Price:- RS:30000 </li><li  style='color:black;font-style:bold;font-size:25px;text-align: justify;'>Elan Boutique is a premium fashion brand known for its chic and contemporary designs, offering a blend of traditional and modern styles. It specializes in exquisite couture, luxury prêt, and bridal collections, perfect for every occasion. The boutique is celebrated for its fine craftsmanship, rich fabrics, and attention to detail.</li>"
+    }
+    else if(name=="HSY Botique"){
+        document.getElementById("img_5").src="image4.jpg"
+        rehman.innerHTML="<li  style='color:black;font-style:bold;font-size:25px'>Name:- HSY Botique </li> <li  style='color:black;font-style:bold;font-size:25px'>Price:- RS:22500 </li><li  style='color:black;font-style:bold;font-size:25px;text-align: justify;'>HSY Boutique, founded by renowned designer Hassan Shehryar Yasin, is famous for its luxurious and haute couture creations. The boutique offers a wide range of bespoke bridal, formal, and prêt collections, blending tradition with modern elegance. HSY is celebrated for its intricate designs, high-quality fabrics, and impeccable craftsmanship.</li>"
+    }
+    else if(name=="Zara Shahjahan Botique"){
+        document.getElementById("img_5").src="Screenshot_2023-05-29-23-32-34-73_1c337646f29875672b5a61192b9010f9_800x.webp"
+        rehman.innerHTML="<li  style='color:black;font-style:bold;font-size:25px'>Name:- Zara Shahjahan Botique </li> <li  style='color:black;font-style:bold;font-size:25px'>Price:- RS:32500 </li><li  style='color:black;font-style:bold;font-size:25px;text-align: justify;'>Zara Shahjahan Boutique is known for its elegant and timeless designs, blending traditional and contemporary styles. The boutique offers a stunning collection of bridal, formal, and ready-to-wear outfits, crafted with intricate details and fine fabrics. Zara Shahjahan's creations reflect sophistication and grace, making it a favorite among fashion enthusiasts.</li>"
+    }
+    else if(name=="Maheen Khan Botique"){
+        document.getElementById("img_5").src="imgg5.jpg"
+        rehman.innerHTML="<li  style='color:black;font-style:bold;font-size:25px'>Name:- Maheen Khan Botique </li> <li  style='color:black;font-style:bold;font-size:25px'>Price:- RS: 5500  </li><li  style='color:black;font-style:bold;font-size:25px;text-align: justify;'>Maheen Khan Boutique is a prestigious fashion label known for its elegant and sophisticated designs. The boutique offers a stunning range of bridal, couture, and ready-to-wear collections, combining traditional craftsmanship with modern aesthetics. Maheen Khan's creations are celebrated for their intricate details, luxurious fabrics, and timeless appeal.</li> "
+    }
+
+
+}
+
+
+
+
---
+++ export.js
@@ -0,0 +1,167 @@
+var header=` <!-- ------ Navbar ------ -->
+   <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+    <div class="container">
+      <a class="navbar-brand" href="#"><img src="Neutral_Elegant_Minimalist_Jewelry_Logo__1_-removebg-preview.png" alt="" id="logo1"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="home.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about us.html">ABOUT</a>
+          </li>
+         
+          
+         <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="gallery.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Gallery
+        </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="gallery.html">Sana Safinaz</a></li>
+                  <li><a class="dropdown-item" href="gallery.html">Elan Botique</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="gallery.html">Zara Shahjahan Botique</a></li>
+                  <li><a class="dropdown-item" href="gallery.html">Maheen Khan Botique</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link  " href="Contact.html" tabindex="-1" aria-disabled="true">CONTACT</a>
+              </li>
+            </ul>
+            <!-- <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form> -->
+          </div>
+        </div>
+      </nav>
+    
+</div>
+<!-- nav bar end --> `
+
+var footer=`<!-- Footer -->
+<footer class="text-center text-lg-start c text-white mt-5">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+      <!-- Left -->
+  
+      <!-- Left -->
+  
+      <!-- Right -->
+      <div>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-google"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+  
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              <i class="fas fa-gem me-3"></i>Madam Botique
+            </h6>
+            <p>
+              Introduction house of Madame, after working for more than a decade, launched their own brand Madame. It has now 13 branches either stand alone or in collaboration with multibrand stores in different cities of Pakistan.
+
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Products
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Sana Safinaz Botique</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Elan Botique</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">HSY Botique</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Zara Shahjahan Botique</a>
+              <p>
+                <a href="#!" class="text-reset">Maheen Khan Botique</a>
+              </p>
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Useful links
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Home</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">#About</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">#Gallery</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">#Contact</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+            <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+            <p>
+              <i class="fas fa-envelope me-3"></i>
+              info@Madamebotique.com
+            </p>
+            <p><i class="fas fa-phone me-3"></i> +92 3472058472</p>
+            <p><i class="fas fa-print me-3"></i> +92 3472058472</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+  
+    <!-- Copyright -->
+    
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->`
+
+
+export {header,footer} 
